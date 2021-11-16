c9fc1458ec3b28d3efc0ddfe68bd655e42bcc43b
Fauzankamal <60697806+Fauzankamal@users.noreply.github.com>
2021-11-16 07:09:36 +0700

    Post Model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,11 +2,9 @@
 
 @section('container')
 <article>
-  <h2>{{ $post["title"] }}</h2>
-  <h5>{{ $post["author"] }}</h5>
-  <p>{{ $post["body"] }}</p>
-  <h5>Author</h5>
-  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident, sint ullam? Voluptas iste architecto quibusdam optio magni obcaecati fugit, velit omnis laborum aperiam, minima rerum maiores consequuntur, ut incidunt cupiditate.</p>
+  <h2>{{ $post->title }}</h2>
+  {!! $post->body !!}
+
 </article>
 
 <a href="/blog">Back to Posts</a>
